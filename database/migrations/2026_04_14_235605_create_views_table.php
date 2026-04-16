868aeb051dcf1c2c779eba97cc80b1893a8ab2b9
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2026-04-16 14:33:26 -0300

    up

--- database/migrations/2026_04_14_235605_create_views_table.php
+++ database/migrations/2026_04_14_235605_create_views_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('course_id')->nullable()->constrained()->nullOnDelete();
             $table->foreignId('lesson_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('block_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('block_id')->nullable()->constrained('lesson_blocks')->cascadeOnDelete();
             $table->boolean('completed')->default(true);
             $table->timestamps();
 
@@ -20,7 +20,6 @@ return new class extends Migration {
         });
     }
 
-    
     public function down(): void {
         Schema::dropIfExists('views');
     }
